15db674fc9e74ed8ee5e2cb421c3e18c3124a44a
LucileSD <4316@holbertonschool.com>
2023-06-29 14:51:35 +0200

    general button

---
+++ cinema/src/components/general/Button.js
@@ -0,0 +1,23 @@
+import './general.css';
+import PropTypes from 'prop-types';
+
+export default function Button(props) {
+  const { text, className, onClick, icon } = props;
+  return (
+    <button type='button' className={className} onClick={() => onClick}>{ icon && <FontAwesomeIcon icon={icon}/>}{text}</button>
+  )
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  icon: PropTypes.element,
+};
+
+Button.defaultProps = {
+  text: '',
+  className: '',
+  onClick: () => {},
+  icon: null,
+};
