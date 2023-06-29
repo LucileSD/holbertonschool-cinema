aa59bc745b276741a5e3a83a76e334f74b468b83
LucileSD <4316@holbertonschool.com>
2023-06-29 14:10:51 +0200

    general Input

---
+++ cinema/src/components/general/Input.js
@@ -0,0 +1,38 @@
+import './general.css';
+import PropTypes from 'prop-types';
+
+export default function Input(props) {
+  const { label, type, className, value, setValue, icon, inputAttributes} = props;
+  const handleInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <>
+      { icon && <FontAwesomeIcon icon={icon}/>}
+      <label>{label}</label>
+      <input type={type} className={className} value={value} onChange={(e)=> handleInput(e)} {...inputAttributes}></input>
+    </>
+  )
+};
+
+
+Input.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  className: PropTypes.string,
+  value: PropTypes.any,
+  setValue: PropTypes.func,
+  icon: PropTypes.element,
+  inputAttributes: PropTypes.object,
+};
+
+Input.defaultProps = {
+  label: '',
+  type: '',
+  className: '',
+  value: null,
+  setValue: () => {},
+  icon: null,
+  inputAttributes: {},
+};
