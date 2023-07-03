161dcc57a033226d21e39fa6198c1f0408c9ef26
LucileSD <4316@holbertonschool.com>
2023-07-03 15:08:25 +0200

    defined routes

--- cinema/src/routes/dashboard/Dashboard.js
+++ cinema/src/routes/dashboard/Dashboard.js
@@ -2,16 +2,23 @@ import './dashboard.css';
 import PropTypes from 'prop-types';
 import Header from '../navigation/Header';
 import SideBar from '../../components/navigation/SideBar';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function Dashboard(props) {
   const { userUsername, setIsLoggedIn} = props;
   return (
-    <>
+    <BrowserRouter>
       <div>
         <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn}></Header>
         <SideBar></SideBar>
       </div>
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+        <Route path='/watchlater' element={<WhatchLater/>}/>
+        <Route path='/*' element={<Navigate to='/'/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
